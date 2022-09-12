78e51b00250ff105c306c5c56da7156918c79b19
Zac Smart <zac_chaz@outlook.com>
2022-09-12 17:52:28 +1000

    moved js to assets folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,30 @@
-function initMap() {
-  var input = document.getElementById('searchTextField');
-  var autocomplete = new google.maps.places.Autocomplete(input);
-  autocomplete.addListener('place_changed', function() {
-    var place = autocomplete.getPlace();
-    document.getElementById("address").value = JSON.stringify(place.address_components);
-  });
+// function initMap() {
+//   var input = document.getElementById('#searchTextField');
+//   var autocomplete = new google.maps.places.Autocomplete(input);
+//   autocomplete.addListener('place_changed', function() {
+//     var place = autocomplete.getPlace();
+//     document.getElementById("address").value = JSON.stringify(place.address_components);
+//   });
+// }
+let autocomplete;
+function initAutocomplete() {
+  autocomplete= new google.maps.places.Autocomplete(
+    document.getElementById('autocomplete'),
+    {
+      componentRestrictions: {'country' : ['AU']},
+      fields: ['place_id', 'geometry', 'name']
+    });
+  autocomplete.addEventListener('on_submit', onPlaceChanged);
 }
-
+function onSubmit() {
+var place = autocomplete.getPlace();
+if (!place.geometry){
+  document.getElementById('autocomplete').placeholder =
+  'Search Here';
+}else{
+  document.getElementById('details').innerHTML = place.name;
+}
+}
 
 
 
@@ -24,30 +42,30 @@ var saveBtn = document.querySelector(".saveBtn");
 // console.log(submitBtnEl);
 
 
-closeModal.addEventListener("click", function () {
-  modal.classList.remove("is-active");
-});
+// closeModal.addEventListener("click", function () {
+//   modal.classList.remove("is-active");
+// });
 
-$("#viewListing1").on("click", function () {
-  modal.classList.add("is-active");
-});
-$("#viewListing2").on("click", function () {
-  modal.classList.add("is-active");
-});
-$("#viewListing3").on("click", function () {
-  modal.classList.add("is-active");
-});
-$("#viewListing4").on("click", function () {
-  modal.classList.add("is-active");
-});
+// $("#viewListing1").on("click", function () {
+//   modal.classList.add("is-active");
+// });
+// $("#viewListing2").on("click", function () {
+//   modal.classList.add("is-active");
+// });
+// $("#viewListing3").on("click", function () {
+//   modal.classList.add("is-active");
+// });
+// $("#viewListing4").on("click", function () {
+//   modal.classList.add("is-active");
+// });
 
-$("#viewListing5").on("click", function () {
-  modal.classList.add("is-active");
-});
+// $("#viewListing5").on("click", function () {
+//   modal.classList.add("is-active");
+// });
 
-$("#viewListing6").on("click", function () {
-  modal.classList.add("is-active");
-});
+// $("#viewListing6").on("click", function () {
+//   modal.classList.add("is-active");
+// });
 
 
 
