39299edc362bf3dd7b6fdd5beaca1e2f7dce3845
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-09-13 23:12:29 +1000

    working out render issue in favs Pages

--- assets/js/listing.js
+++ assets/js/listing.js
@@ -82,6 +82,28 @@ var renderListing = function (listing) {
       renderImg(listing);
       viewModal();
       console.log("viewbutton is clicked");
+    } else if (target.matches("#save-button")) {
+      // The save button is disabled if the listing is already saved to favourites
+      $(target).attr("disabled", true);
+      var myFavourites = JSON.parse(localStorage.getItem("favProp"));
+      //If there are no favourite listings there will be an empty array
+      if (myFavourites == null) {
+        myFavourites = [];
+      }
+      var isAlreadyFavourited = false;
+
+      for (var i = 0; i < myFavourites.length; i++) {
+        var favouriteListing = myFavourites[i];
+        if (favouriteListing.id === listing.id) {
+          isAlreadyFavourited = true;
+        }
+      }
+      if (!isAlreadyFavourited) {
+        myFavourites.push(listing);
+        localStorage.setItem("favProp", JSON.stringify(myFavourites));
+      } else {
+        console.log("Listing already in favourites", listing);
+      }
     }
   });
 };
@@ -137,7 +159,7 @@ var initialize = function () {
   if (postcode) {
     getListing(postcode);
   }
-}
+};
 
 $(document).ready(function () {
   initialize();
@@ -149,9 +171,96 @@ $(document).ready(function () {
   });
 });
 
-
 function navigateToListing(postcode) {
-  document.location.assign("./listings.html?postcode=" + postcode)
+  document.location.assign("./listings.html?postcode=" + postcode);
 }
 
 window.navigateToListing = navigateToListing;
+
+var loadFavourites = function () {
+  console.log("Restoring array data from local storage");
+  var myFavourites = JSON.parse(localStorage.getItem("favProp"));
+  console.log(myFavourites);
+  if (myFavourites === null) {
+    myFavourites = [];
+  }
+  for (let i = 0; i < myFavourites.length; i++) {
+    const favouriteListing = myFavourites[i];
+    renderFavourite(favouriteListing);
+  }
+};
+
+var renderFavourite = function (favouriteListing) {
+  console.log("Restoring array data from local storage");
+  console.log(favouriteListing);
+  var listingEl = $("<div>").addClass("tile is-parent").html(`
+  <div class="tile is-child box">
+      <p class="title">${favouriteListing.propertyDetails.allPropertyTypes}</p>
+      <div class="subtitle">
+      <ul>
+          <li>${favouriteListing.propertyDetails.displayableAddress}</li>
+          <li>${favouriteListing.headline}</li>
+      </ul>
+      </div>
+      <div class="content">
+      <ul>
+          <li>${favouriteListing.priceDetails.displayPrice}</li>
+          <li>${favouriteListing.advertiser.name}</li>
+          <img src=${favouriteListing.media[0].url}/>
+      </ul>
+      <footer>
+          <button id="view-button" class="button">view</button>
+          <button id="remove-button" class="button">remove</button>
+      </footer>
+      </div>
+  </div>
+  `);
+
+  //event del to save button
+  $("#fav-listings").append(listingEl);
+
+  listingEl.on("click", function (e) {
+    e.preventDefault();
+    console.log(e);
+    const target = e.target;
+    if (target.matches("#view-button")) {
+      renderImg(listing);
+      viewModal();
+      console.log("viewbutton is clicked");
+    } else if (target.matches("#remove-button")) {
+      removeFromFavourites(favouriteListing.id);
+      listingEl.remove();
+      // 5. delete the element off the page - to do so, you need to query select the container and use the .remove() method to remove it from DOM
+    }
+  });
+};
+
+//Remove a listing from favourites
+var removeFromFavourites = function (listingIdToBeDeleted) {
+  // 1. load the existing favs (existingFavs)
+  var existingFavs = JSON.parse(localStorage.getItem("favProp"));
+  // 2. create a newFavs array and initialize to an empty array []
+  if (newFavs == null) {
+    newFavs = [];
+  }
+  // 3. loop through existingFavs and if the fav.id does not match listingIdToBeDeleted -> then push the fav into the newFavs array
+  for (let i = 0; i < newFavs; i++) {
+    const newFavs = listingIdToBeDeleted[i];
+    renderFavourite(newFavs);
+  }
+  // 4. at the end json parse the newFavs and set to local storage
+  locateStorage.clear("");
+
+  console.log("delete");
+};
+
+// var
+
+//Clear all favourites - add a main button
+// clearEl.addEventListener("click", function () {
+//   localStorage.clear();
+//   searchHistory = [];
+//   renderSearchHistory();
+// })
+
+loadFavourites();
